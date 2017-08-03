3aa72b7585c64e92322bbbc7b259af8db4866392
Linnea Benoit <linneabenoit@gmail.com>
2017-08-03 16:28:21 -0400

    restore

---
+++ js/quotes.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+  // Convenience function for inserting innerHTML for 'select'
+  var insertHtml = function(selector, html) {
+      var targetElem = document.getElementById(selector);
+      targetElem.innerHTML = html;
+  };
+
+  var generateQuote = function(){
+    var quoteText = "";
+    var quoteAuthor = "";
+    var responseObj;
+
+    $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en").done (function(data) {
+      var jsonObj = JSON.parse(data);
+      quoteText = jsonObj.quoteText;
+      quoteAuthor = jsonObj.quoteAuthor;
+    });
+
+    return quoteText;
+  };
+
+  insertHtml ("quote", generateQuote());
+
+  $("button").on("click", function(){
+
+    generateQuote();
+
+    });
+
+//    $("quote").html();
+  });
