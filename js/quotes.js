b7fe8db5b6648598823260ccbceb70b547e90717
Linnea Benoit <linneabenoit@gmail.com>
2017-08-03 15:11:00 -0400

    'changes'

--- js/quotes.js
+++ js/quotes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function() {
 
   // Convenience function for inserting innerHTML for 'select'
@@ -29,39 +28,5 @@ $(document).ready(function() {
 
     });
 
-    $("quote").html();
+//    $("quote").html();
   });
-=======
-$(document).ready(function() {
-
-  // Convenience function for inserting innerHTML for 'select'
-  var insertHtml = function(selector, html) {
-      var targetElem = document.getElementById(selector);
-      targetElem.innerHTML = html;
-  };
-
-  var generateQuote = function(){
-    var quoteText = "";
-    var quoteAuthor = "";
-    var responseObj;
-
-    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en").done (function(data) {
-      var jsonObj = JSON.parse(data);
-      quoteText = jsonObj.quoteText;
-      quoteAuthor = jsonObj.quoteAuthor;
-    });
-
-    return quoteText;
-  };
-
-  insertHtml ("quote", generateQuote());
-
-  $("button").on("click", function(){
-
-    generateQuote();
-
-    });
-
-    $("quote").html();
-  });
->>>>>>> 55b2ea515a9a9b8ff9aff5d0411383ead46642fb
