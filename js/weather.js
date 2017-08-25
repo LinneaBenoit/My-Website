0c3aad77de9eee92667a0c550d53c653ac20ff55
Linnea Benoit <linneabenoit@gmail.com>
2017-08-25 17:42:02 -0400

    weather

---
+++ js/weather.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+  function getLocation() {
+      if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+        //fix this!!
+          x.innerHTML = "Geolocation is not supported by this browser.";
+      }
+  }
+
+  function showPosition(position) {
+//      console.log("https://fcc-weather-api.glitch.me/api/current?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude);
+
+
+      $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude)
+        .done(function(data) {
+          $("#location-name").text(data.name);
+//          console.log();
+          $("#location-temp").html(Math.round(convertCelcius(data.main.temp)));
+          $("#scale").html("F");
+          $("#location-conds").html(data.weather[0].description);
+          $("#location-image").attr('src', data.weather[0].icon);
+          $("#location-image").attr('alt', data.weather[0].description);
+        });
+
+  }
+
+  function convertCelcius(cTemp) {
+    return cTemp * 1.8 + 32;
+  }
+
+  function convertFarenheit(fTemp) {
+    return (fTemp - 32) / 1.8;
+  }
+
+  function changeScale(temp, scale) {
+    console.log("scale is " + scale);
+    if (scale.localeCompare("F") == 0) { //currently in farenheit
+      $("#location-temp").html(Math.round(convertFarenheit(temp)));
+      $("#scale").html("C");
+    } else {
+      $("#location-temp").html(Math.round(convertCelcius(temp)));
+      $("#scale").html("F");
+    };
+  }
+
+  $("#scale").on('click', function() {
+    changeScale($("#location-temp").html(), $("#scale").html());
+});
+
+  getLocation();
+
+});
